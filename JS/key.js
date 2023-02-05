df92e425011c20749c37b1f10fb8531ec1273e9d
Antony Lucas <54651622+Antony-Lucas@users.noreply.github.com>
2023-02-05 16:59:57 -0300

    Update key.js

--- JS/key.js
+++ JS/key.js
@@ -1,2 +1,2 @@
-const api_key = "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";
+const api_key = "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";
 const url_openAi = "https://api.openai.com/v1/completions";
