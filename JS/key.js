4e2fd4c9bba6b17a0ba0e9d5637f4535cfad80dc
Antony Lucas <54651622+Antony-Lucas@users.noreply.github.com>
2023-02-05 17:04:05 -0300

    Update key.js

--- JS/key.js
+++ JS/key.js
@@ -1,2 +1,2 @@
-const api_key = "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";
-const url_openAi = "https://api.openai.com/v1/completions";
+const api_key = "404";
+const url_openAi = "400";
